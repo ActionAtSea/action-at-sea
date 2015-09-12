dd21270e5eee0c65d03cea990345751ffc8688be
Kara Jensen <mail@karajensen.com>
2015-09-12 18:15:04 +0800

    adding test 3D fog

---
+++ Assets/Scripts/ParticleFogOfWar.cs
@@ -0,0 +1,73 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+// Action At Sea - ParticleFogOfWar.cs
+////////////////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using System.Collections;
+
+public class ParticleFogOfWar : MonoBehaviour 
+{    
+    private ParticleSystem m_particles;
+    private bool m_fade = false;
+    private Color m_colour = new Color(0.294f, 0.294f, 0.353f, 1.0f);
+    private bool m_initialised = false;
+
+    /// <summary>
+    /// Initialises the particle effect
+    /// </summary>
+    void Start () 
+    {
+        m_particles = GetComponent<ParticleSystem>();
+    }
+
+    /// <summary>
+    /// On collision with the player
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_fade = true;
+        }
+    }
+
+    /// <summary>
+    /// Enables/disables collision detection
+    /// </summary>
+    void Update()
+    {
+        m_particles.GetComponent<SphereCollider>().enabled = 
+            PlayerPlacer.IsCloseToPlayer(m_particles.transform.position);
+
+        if(m_fade || !m_initialised)
+        {
+            if(m_initialised)
+            {
+                m_colour.r -= Time.deltaTime * 0.15f;
+                m_colour.g -= Time.deltaTime * 0.15f;
+                m_colour.b -= Time.deltaTime * 0.15f;
+                m_colour.a -= Time.deltaTime * 0.7f;
+            }
+
+            m_initialised = true;
+
+            if(m_colour.a <= 0.0f)
+            {
+                m_particles.gameObject.SetActive(false);
+            }
+            else
+            {
+                ParticleSystem.Particle[] particles = new ParticleSystem.Particle[m_particles.particleCount];
+                m_particles.GetParticles(particles);
+
+                for(int i = 0; i < particles.Length; ++i)
+                {
+                    particles[i].size -= Time.deltaTime * 2.0f;
+                    particles[i].color = m_colour;
+                }
+
+                m_particles.SetParticles(particles, m_particles.particleCount);
+            }
+        }
+    }
+}
