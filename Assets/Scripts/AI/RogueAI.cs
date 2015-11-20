ec76685239ce2f99c06133b13630b2b56ec495a0
Tronobro <reginald.ashman@students.aie.edu.au>
2015-11-20 22:22:02 +1030

    Created Rogue AI class and prefab (not fully implemented)

---
+++ Assets/Scripts/AI/RogueAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RogueAI : MonoBehaviour 
+{
+  
+    private NavMeshAgent navAgent = null;
+	// Use this for initialization
+	void Start () 
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        if(navAgent != null)
+        {
+            navAgent.SetDestination(new Vector3(10, 1, 100));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
