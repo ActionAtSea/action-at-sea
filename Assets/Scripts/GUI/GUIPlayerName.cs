0d97cf1c831991900cb1b35c9c56ac3ee564830f
Tronobro <reginald.ashman@students.aie.edu.au>
2015-12-17 16:10:53 +1030

    Fixed player name

--- Assets/Scripts/GUI/GUIPlayerName.cs
+++ Assets/Scripts/GUI/GUIPlayerName.cs
@@ -19,11 +19,17 @@ public class GUIPlayerName : MonoBehaviour
 
     void Start()
     {
-        //TODO: remove back text as it doesn't seem to do anything?
+        //TODO: back text doesn't seem to be doing anything?
         m_backtext = transform.parent.gameObject.GetComponent<UnityEngine.UI.Text>();
         m_text = GetComponent<UnityEngine.UI.Text>();
         m_text.text = "";
         m_backtext.text = m_text.text;
+    }
+
+    //Used to set the player name when the input field is deselected.
+    public void SetName(string name)
+    {
+        Utilities.SetPlayerName(name);
     }
 
     void Update()
